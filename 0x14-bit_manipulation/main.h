b1a5d39b9e76a742bb66dc23234e1519a3dccc53
Shamsdojja <khadija90lamhamdi@gmail.com>
2023-07-07 11:01:14 -0700

    0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #ifdef DEBUG /*prints debug statements*/
 #endif
-int _putchar(charc);
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
